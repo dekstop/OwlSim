dccc7818bc41dad4573f34a619565f3039f1b9a1
martind <martin@dekstop.de>
2013-06-10 01:47:11 +0100

    Dual Tremolo patch

--- Source/PatchRegistry.cpp
+++ Source/PatchRegistry.cpp
@@ -2,12 +2,14 @@
 
 #include "Patches/GainPatch.hpp"
 #include "Patches/TemplatePatch.hpp"
+#include "Patches/DualTremoloPatch.hpp"
 
 #define REGISTER_PATCH(T, STR) registerPatch(STR, Register<T>::construct)
 
 PatchRegistry::PatchRegistry(){
   REGISTER_PATCH(GainPatch, "Gain");
-  REGISTER_PATCH(TemplatePatch, "Template");
+    REGISTER_PATCH(TemplatePatch, "Template");
+    REGISTER_PATCH(DualTremoloPatch, "Dual Tremolo");
 }
 
 StringArray PatchRegistry::getNames(){
