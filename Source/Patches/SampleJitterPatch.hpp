7209b60696b5a62cb98a19fce18a0876f4f3b612
martind <martin@dekstop.de>
2013-06-14 13:46:53 +0100

    4 spaces -> 2 spaces.

--- Source/Patches/SampleJitterPatch.hpp
+++ Source/Patches/SampleJitterPatch.hpp
@@ -26,75 +26,75 @@
 //#include "juce_Random.h"
 
 class SampleJitterPatch : public Patch {
-    
-    const float MIN_DELAY = 0.00001; // in seconds
-    const float MAX_DELAY = 0.02;
-    const float MIN_BIAS = 0.1; // bias acts as exponent on a random delay time, t = rnd^bias
-    const float MAX_BIAS = 6;
-    
-    float* circularBuffer;
-    unsigned int bufferSize;
-    unsigned int writeIdx;
-    
+  
+  const float MIN_DELAY = 0.00001; // in seconds
+  const float MAX_DELAY = 0.02;
+  const float MIN_BIAS = 0.1; // bias acts as exponent on a random delay time, t = rnd^bias
+  const float MAX_BIAS = 6;
+  
+  float* circularBuffer;
+  unsigned int bufferSize;
+  unsigned int writeIdx;
+  
 public:
-    void processAudio(AudioInputBuffer &input, AudioOutputBuffer &output){
+  void processAudio(AudioInputBuffer &input, AudioOutputBuffer &output){
 
-        double rate = getSampleRate();
-        
-        if (circularBuffer==NULL)
-        {
-            bufferSize = MAX_DELAY * rate;
-            circularBuffer = new float[bufferSize];
-            writeIdx = 0;
-        }
-
-        float p1 = getRampedParameterValue(PARAMETER_A);
-        float p2 = getRampedParameterValue(PARAMETER_B);
-//        float p3 = getRampedParameterValue(PARAMETER_C);
-        float p4 = getRampedParameterValue(PARAMETER_D);
-
-        unsigned int maxSampleDelay = rate * (MIN_DELAY + p1*p1 * (MAX_DELAY-MIN_DELAY));
-        float bias = MIN_BIAS + p2*p2 * (MAX_BIAS-MIN_BIAS);
-        // float cutoff = p3;
-        float dryWetMix = p4;
-        
-        int size = input.getSize();
-        float* buf = input.getSamples();
-        Random r;
-        for (int i=0; i<size; ++i)
-        {
-            int offset = round(maxSampleDelay * pow(r.nextFloat(), bias));
-            int readIdx = writeIdx - offset;
-            while (readIdx<0)
-                readIdx += bufferSize;
-
-            circularBuffer[writeIdx] = buf[i];
-            buf[i] =
-                circularBuffer[readIdx] * dryWetMix +
-                buf[i] * (1 - dryWetMix);
-
-            writeIdx = (++writeIdx) % bufferSize;
-        }
-        output.setSamples(buf);
-    }
+    double rate = getSampleRate();
     
-    ~SampleJitterPatch(){
-        free(circularBuffer);
+    if (circularBuffer==NULL)
+    {
+      bufferSize = MAX_DELAY * rate;
+      circularBuffer = new float[bufferSize];
+      writeIdx = 0;
     }
 
+    float p1 = getRampedParameterValue(PARAMETER_A);
+    float p2 = getRampedParameterValue(PARAMETER_B);
+//    float p3 = getRampedParameterValue(PARAMETER_C);
+    float p4 = getRampedParameterValue(PARAMETER_D);
+
+    unsigned int maxSampleDelay = rate * (MIN_DELAY + p1*p1 * (MAX_DELAY-MIN_DELAY));
+    float bias = MIN_BIAS + p2*p2 * (MAX_BIAS-MIN_BIAS);
+    // float cutoff = p3;
+    float dryWetMix = p4;
     
+    int size = input.getSize();
+    float* buf = input.getSamples();
+    Random r;
+    for (int i=0; i<size; ++i)
+    {
+      int offset = round(maxSampleDelay * pow(r.nextFloat(), bias));
+      int readIdx = writeIdx - offset;
+      while (readIdx<0)
+        readIdx += bufferSize;
+
+      circularBuffer[writeIdx] = buf[i];
+      buf[i] =
+        circularBuffer[readIdx] * dryWetMix +
+        buf[i] * (1 - dryWetMix);
+
+      writeIdx = (++writeIdx) % bufferSize;
+    }
+    output.setSamples(buf);
+  }
+  
+  ~SampleJitterPatch(){
+    free(circularBuffer);
+  }
+
+  
 private:
-    // Parameter ramping to reduce clicks.
-    
-    float oldVal[4] = {0, 0, 0, 0};
-    float ramp = 0.1; // 0..1
-    
-    float getRampedParameterValue(PatchParameterId id){
-        float val = getParameterValue(id);
-        float result = val * ramp + oldVal[id] * (1-ramp);
-        oldVal[id] = val;
-        return result;
-    }
+  // Parameter ramping to reduce clicks.
+  
+  float oldVal[4] = {0, 0, 0, 0};
+  float ramp = 0.1; // 0..1
+  
+  float getRampedParameterValue(PatchParameterId id){
+    float val = getParameterValue(id);
+    float result = val * ramp + oldVal[id] * (1-ramp);
+    oldVal[id] = val;
+    return result;
+  }
 };
 
 #endif // OwlSim_SampleJitterPatch_hpp
--- Source/Patches/SimpleDelayPatch.hpp
+++ Source/Patches/SimpleDelayPatch.hpp
@@ -17,6 +17,7 @@
 //  - clear buffer tail when shortening delay time
 //    - OR: always write to full buffer, but stretch it (and adjust read/write speed)
 //  - better dry/wet mixing method (don't just fade over)
+//  - this is noisy as fuck, esp for short delay times; not entirely sure why.
 //
 
 #ifndef OwlSim_SimpleDelay_hpp
@@ -25,73 +26,73 @@
 #include "StompBox.h"
 
 class SimpleDelayPatch : public Patch {
-    
-    const float MIN_DELAY = 0.01; // in seconds
-    const float MAX_DELAY = 8;
-    
-    float* circularBuffer;
-    unsigned int bufferSize;
-    unsigned int writeIdx;
-    
+  
+  const float MIN_DELAY = 0.01; // in seconds
+  const float MAX_DELAY = 8;
+  
+  float* circularBuffer;
+  unsigned int bufferSize;
+  unsigned int writeIdx;
+  
 public:
-    void processAudio(AudioInputBuffer &input, AudioOutputBuffer &output){
+  void processAudio(AudioInputBuffer &input, AudioOutputBuffer &output){
 
-        double rate = getSampleRate();
-        
-        if (circularBuffer==NULL)
-        {
-            bufferSize = MAX_DELAY * rate;
-            circularBuffer = new float[bufferSize];
-            writeIdx = 0;
-        }
-
-        float p1 = getRampedParameterValue(PARAMETER_A);
-        float p2 = getRampedParameterValue(PARAMETER_B);
-//        float p3 = getRampedParameterValue(PARAMETER_C);
-        float p4 = getRampedParameterValue(PARAMETER_D);
-
-        unsigned int sampleDelay = (MIN_DELAY + p1 * (MAX_DELAY-MIN_DELAY)) * rate;
-        float feedback = p2;
-        // float cutoff = p3;
-        float dryWetMix = p4;
-        
-        int size = input.getSize();
-        float* buf = input.getSamples();
-        int readIdx = writeIdx - sampleDelay;
-        while (readIdx<0)
-            readIdx += bufferSize;
-        for (int i=0; i<size; ++i)
-        {
-            circularBuffer[writeIdx] =
-                circularBuffer[writeIdx] * feedback +
-                buf[i] * (1 - feedback);
-            buf[i] =
-                circularBuffer[readIdx] * dryWetMix +
-                buf[i] * (1 - dryWetMix);
-
-            writeIdx = (++writeIdx) % sampleDelay;
-            readIdx = (++readIdx) % sampleDelay;
-        }
-        output.setSamples(buf);
-    }
+    double rate = getSampleRate();
     
-    ~SimpleDelayPatch(){
-        free(circularBuffer);
+    if (circularBuffer==NULL)
+    {
+      bufferSize = MAX_DELAY * rate;
+      circularBuffer = new float[bufferSize];
+      writeIdx = 0;
     }
 
+    float p1 = getRampedParameterValue(PARAMETER_A);
+    float p2 = getRampedParameterValue(PARAMETER_B);
+//    float p3 = getRampedParameterValue(PARAMETER_C);
+    float p4 = getRampedParameterValue(PARAMETER_D);
+
+    unsigned int sampleDelay = (MIN_DELAY + p1 * (MAX_DELAY-MIN_DELAY)) * rate;
+    float feedback = p2;
+    // float cutoff = p3;
+    float dryWetMix = p4;
     
+    int size = input.getSize();
+    float* buf = input.getSamples();
+    int readIdx = writeIdx - sampleDelay;
+    while (readIdx<0)
+      readIdx += bufferSize;
+    for (int i=0; i<size; ++i)
+    {
+      circularBuffer[writeIdx] =
+        circularBuffer[writeIdx] * feedback +
+        buf[i] * (1 - feedback);
+      buf[i] =
+        circularBuffer[readIdx] * dryWetMix +
+        buf[i] * (1 - dryWetMix);
+
+      writeIdx = (++writeIdx) % sampleDelay;
+      readIdx = (++readIdx) % sampleDelay;
+    }
+    output.setSamples(buf);
+  }
+  
+  ~SimpleDelayPatch(){
+    free(circularBuffer);
+  }
+
+  
 private:
-    // Parameter ramping to reduce clicks.
-    
-    float oldVal[4] = {0, 0, 0, 0};
-    float ramp = 0.1; // 0..1
-    
-    float getRampedParameterValue(PatchParameterId id){
-        float val = getParameterValue(id);
-        float result = val * ramp + oldVal[id] * (1-ramp);
-        oldVal[id] = val;
-        return result;
-    }
+  // Parameter ramping to reduce clicks.
+  
+  float oldVal[4] = {0, 0, 0, 0};
+  float ramp = 0.1; // 0..1
+  
+  float getRampedParameterValue(PatchParameterId id){
+    float val = getParameterValue(id);
+    float result = val * ramp + oldVal[id] * (1-ramp);
+    oldVal[id] = val;
+    return result;
+  }
 };
 
 #endif // OwlSim_SimpleDelay_hpp
